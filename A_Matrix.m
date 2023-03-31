94e44bacfaf26706c81eb44096ede2f24ad843b3
RyanTuppen <35409648+RyanTuppen@users.noreply.github.com>
2023-03-31 13:49:11 +0100

    Add files via upload

---
+++ A_Matrix.m
@@ -0,0 +1,16 @@
+function [A] = A_Matrix(n,alpha,dx) %n = dimension of A
+%alpha = constant
+A = zeros(n,n); %dx = gridspace resolution
+for i = 2:n-1
+    A(i,i-1) = 1/(dx*dx); %setting values for A excluding
+    A(i,i) = -alpha - 2/(dx*dx); %A 1i and A ni
+    A(i,i+1) = 1/(dx*dx);
+end
+A(1,1) = -alpha -2/(dx*dx);
+A(1,2) = 1/(dx*dx); %setting values for the 1st row and
+A(1,n) = 1/(dx*dx); %nth row
+A(n,1) = 1/(dx*dx);
+A(n,n-1) = 1/(dx*dx);
+A(n,n) = -alpha - 2/(dx*dx);
+end
+
